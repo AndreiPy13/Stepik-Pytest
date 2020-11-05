ed4c935fc66201527afec293c3926f0f83b95bd4
AndreiPy13 <sniper379@mail.ru>
2020-11-05 16:25:54 +0700

    create new locators for check

--- pages/locators.py
+++ pages/locators.py
@@ -4,9 +4,18 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
 	LOGIN_NAME_FORM = (By.CSS_SELECTOR, "#id_login-username")
 	LOGIN_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_login-password")
 	REGISTRATION_EMAIL_FORM = (By.CSS_SELECTOR, "#id_registration-email")
 	REGISTRATION_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_registration-password1")
 	REGISTRATION_RETURN_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
+class ProductPageLocators():
+	BASKET_FORM = (By.CSS_SELECTOR, ".btn-add-to-basket")
+	NAME_IN_NOTIFICATION = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+	NAME_OF_PRODUCT = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
+	PRICE_IN_NOTIFICATION = (By.CSS_SELECTOR, ".alert-info.fade.in > div > p:nth-child(1) > strong") 
+	PRICE_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main > p.price_color")
